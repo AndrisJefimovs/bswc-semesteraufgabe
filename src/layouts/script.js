1f365bac4ed62b4bf9a18bc4c9608bfbea1fb698
Andris Jefimovs <58335696+AndrisJefimovs@users.noreply.github.com>
2024-05-08 09:46:04 +0200

    Add POST form functionality

--- src/layouts/script.js
+++ src/layouts/script.js
@@ -2,7 +2,12 @@ const inputType = document.querySelector("#input-type");
 const inputName = document.querySelector("#input-name");
 const cbStart = document.querySelector("#cb-start");
 const inputStart = document.querySelector("#input-start");
+const cbEnd = document.querySelector("#cb-end");
+const inputEnd = document.querySelector("#input-end");
+const inputCustom = document.querySelector("#input-custom");
 const btnNewPackage = document.querySelector("#btn-new-package");
+
+const outputField = document.querySelector("#entries");
 
 btnNewPackage.onclick = async e => {
 	e.preventDefault();
@@ -10,10 +15,19 @@ btnNewPackage.onclick = async e => {
 	const formData = new FormData();
 	formData.append("type", inputType.value);
 	formData.append("name", inputName.value);
+	formData.append("start", cbStart.value);
+	formData.append("startVal", inputStart.value);
+	formData.append("end", cbEnd.value);
+	formData.append("endVal", inputEnd.value);
+	formData.append("custom", inputCustom.value);
 
 	fetch("../cgi-bin/update.cgi", {
 		method: "POST",
 		body: formData
+	})
+	.then(data => data.text())
+	.then(markup => {
+		console.log(markup);
 	});
 }
 
