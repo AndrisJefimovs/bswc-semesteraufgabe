bdfa1c5ae5316d5d9f0e699722f024f662f7f5a2
Andris Jefimovs <58335696+AndrisJefimovs@users.noreply.github.com>
2024-05-08 02:06:00 +0200

    Initial POST form functionality

--- src/layouts/script.js
+++ src/layouts/script.js
@@ -1,1 +1,19 @@
-document.body.innerHTML = '<small style="color: orange;">JS geladen</small>' + document.body.innerHTML;
+const inputType = document.querySelector("#input-type");
+const inputName = document.querySelector("#input-name");
+const cbStart = document.querySelector("#cb-start");
+const inputStart = document.querySelector("#input-start");
+const btnNewPackage = document.querySelector("#btn-new-package");
+
+btnNewPackage.onclick = async e => {
+	e.preventDefault();
+
+	const formData = new FormData();
+	formData.append("type", inputType.value);
+	formData.append("name", inputName.value);
+
+	fetch("../cgi-bin/update.cgi", {
+		method: "POST",
+		body: formData
+	});
+}
+
