310c48ef66eb9163bf7fcbbc1d7b6c100455223a
Andris Jefimovs <58335696+AndrisJefimovs@users.noreply.github.com>
2024-05-21 16:02:17 +0200

    Add file download button

--- src/layouts/script.js
+++ src/layouts/script.js
@@ -13,30 +13,31 @@ const generateBtn = document.querySelector("#btn-generate");
 const outputField = document.querySelector("#output");
 
 const clipboardBtn = document.querySelector("#btn-copy");
+const downloadBtn = document.querySelector("#btn-download");
 
 entries = {
-    packages: [],
-	c: [],
-	r: []
+packages: [],
+		  c: [],
+		  r: []
 };
 
 const addEntry = _ => {
-    entries.packages.push({
-		type: inputType.value,
-        name: inputName.value,
-		startVerDefined: cbStart.checked,
-		startVer: inputStart.value,
-		endVerDef: cbEnd.checked,
-		endVer: inputEnd.value,
-		custom: inputCustom.value
-	});
+	entries.packages.push({
+type: inputType.value,
+name: inputName.value,
+startVerDefined: cbStart.checked,
+startVer: inputStart.value,
+endVerDef: cbEnd.checked,
+endVer: inputEnd.value,
+custom: inputCustom.value
+});
 };
 
 const updateList = _ => {
 	let newMarkup = "";
-    entries.packages.forEach(entry => {
-       newMarkup += `<li><span>${entry.type}</span><span>${entry.name}</span><span>${entry.startVerDefined}</span><span>${entry.startVer}</span><span>${entry.endVerDef}</span><span>${entry.endVer}</span><span>${entry.custom}</span></li>`; 
-	});
+	entries.packages.forEach(entry => {
+			newMarkup += `<li><span>${entry.type}</span><span>${entry.name}</span><span>${entry.startVerDefined}</span><span>${entry.startVer}</span><span>${entry.endVerDef}</span><span>${entry.endVer}</span><span>${entry.custom}</span></li>`; 
+			});
 	entriesField.innerHTML = newMarkup;
 };
 
@@ -44,20 +45,42 @@ btnNewPackage.onclick = e => {e.preventDefault(); addEntry(); updateList();};
 
 generateRequirementsTxt = async _ => {
 	fetch("../cgi-bin/update.cgi", {
-		method: "POST",
-		body: JSON.stringify(entries)
-	})
-	.then(res => res.text())
+method: "POST",
+body: JSON.stringify(entries)
+})
+.then(res => res.text())
 	.then(data => {
-		outputField.value = data;	
-	});
+			outputField.value = data;	
+			});
 };
 
 generateBtn.onclick = generateRequirementsTxt;
 
-btnCopy.onclick = e => {
-    outputField.select();
+clipboardBtn.onclick = e => {
+	outputField.select();
 	outputField.setSelectionRange(0, 99999);
 	navigator.clipboard.writeText(outputField.value);
 };
+
+downloadBtn.onclick = e => {
+	download(outputField.value, "requirements.txt", "text/plain");
+};
+
+function download(data, filename, type) {
+	var a = document.createElement("a"),
+		file = new Blob([data], {type: type});
+	if (window.navigator.msSaveOrOpenBlob) // IE10+
+		window.navigator.msSaveOrOpenBlob(file, filename);
+	else { // Others
+		var url = URL.createObjectURL(file);
+		a.href = url;
+		a.download = filename;
+		document.body.appendChild(a);
+		a.click();
+		setTimeout(function() {
+				document.body.removeChild(a);
+				window.URL.revokeObjectURL(url);  
+				}, 0); 
+	}
+}
 
